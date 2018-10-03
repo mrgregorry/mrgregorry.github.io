beeaa9c053730df5d44782d908e7d4ac6f690800
Greg <43776290+mrgregorry@users.noreply.github.com>
2018-10-04 02:26:07 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ $(document).ready(function(){
    $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
 	    if (scroll > 70 ) {
-	        $(".menu-top").removeClass('bg-inner').addClass('bg-after').addClass('neon');
+	        $(".menu-top").addClass('bg-after').addClass('neon');
 	        console.log('scrolling up !');
 
 	    }else{
-	    	$(".menu-top").removeClass('bg-after').removeClass('neon').addClass('bg-inner');
+	    	$(".menu-top").removeClass('bg-after').removeClass('neon');
 	    	console.log('scrolling down !');
 	    	console.log(scroll);
 	    }
