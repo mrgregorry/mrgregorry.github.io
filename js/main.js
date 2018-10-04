ec98f33a1ad0b900ca22ede02b2bee79d07714c3
Greg <43776290+mrgregorry@users.noreply.github.com>
2018-10-04 15:11:26 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,15 +3,23 @@ $(document).ready(function(){
    $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
 	    if (scroll > 70 ) {
-	        $(".menu-top").removeClass('bg-light').addClass('bg-after').addClass('neon');
-	        console.log('scrolling up !');
+	        $(".menu-top").removeClass('bg-white').addClass('bg-after').addClass('neon');
+	        // console.log('scrolling up !');
 
 	    }else{
-	    	$(".menu-top").removeClass('bg-after').removeClass('neon').addClass('bg-light');
-	    	console.log('scrolling down !');
-	    	console.log(scroll);
+	    	$(".menu-top").removeClass('bg-after').removeClass('neon').addClass('bg-white');
+	    	// console.log('scrolling down !');
+	    	// console.log(scroll);
 	    }
 	});
 
-});
+   $(document).ready(function() {
+		$("#top").click(function(event) {
+		    event.preventDefault();
+		    $("html, body").animate({ scrollTop: 0 }, "slow");
+		    return false;
+		});
+	});
 
+});
+   
